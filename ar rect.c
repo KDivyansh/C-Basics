f804f03cc4ae32611129340f3cd014d6c2220b94
Divyansh Katare <93079133+KDivyansh@users.noreply.github.com>
2022-10-16 12:33:57 +0530

    Area of rectangle

---
+++ ar rect.c
@@ -0,0 +1,20 @@
+/*
+@author:Divyansh Katare
+@description: A program to find area of Rectangle.
+*/
+#include <stdio.h>
+
+int main()
+{
+    float area;
+   float len;
+   printf("Enter the length of rectangle\n");
+   scanf("%f",&len);
+
+   float bre;
+   printf("Enter the breadth of rectangle\n");
+   scanf("%f",&bre);
+   area=len*bre;
+   printf("Area Of rectangle with \nlength %f \nand \nbreadth %f is\n %f",len,bre,area);
+    return 0;
+}Ṇ
