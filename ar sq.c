fda8b7e2cae705845651d0319ab4c44b015b4bf4
Divyansh Katare <93079133+KDivyansh@users.noreply.github.com>
2022-10-16 12:34:50 +0530

    Area Of Square

---
+++ ar sq.c
@@ -0,0 +1,16 @@
+/*
+@author:Divyansh Katare
+@description: A program to find area of Square.
+*/
+#include <stdio.h>
+
+int main()
+{
+    float s,a;
+    printf("Enter the value of side of Square");
+    scanf("%f",&s);
+    a=s*s;
+    printf("Area of the Square with side %f is %f",s,a);
+
+    return 0;
+}
